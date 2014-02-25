2ddeef4b3b54e0bfde4eb561b67c0852a89b2900
David Kapp <davekapp@gmail.com>
2014-02-25 10:46:07 -0600

    working todomvc

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -11,6 +11,12 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       this.set("newTitle", "");
       todo.save();
+    },
+
+    clearCompleted: function() {
+      var completed = this.filterBy("isCompleted", true);
+      completed.invoke("deleteRecord");
+      completed.invoke("save");
     }
   },
 
@@ -21,5 +27,26 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function() {
     var remaining = this.get("remaining");
     return remaining === 1 ? "item" : "items";
-  }.property("remaining")
+  }.property("remaining"),
+
+  hasCompleted: function() {
+    return this.get("completed") > 0;
+  }.property("completed"),
+
+  completed: function() {
+    return this.filterBy("isCompleted", true).get("length");
+  }.property("@each.isCompleted"),
+
+  allAreDone: function(key, value) {
+    console.log("allAreDone; key: " + key + " value: " + value);
+    if (value === undefined) { // comes through as undefined when changed through an event, true or false if it was manually checked
+      return !!this.get("length") && this.everyProperty("isCompleted", true);
+    } else {
+      this.setEach("isCompleted", value);
+      this.invoke("save");
+      return value;
+    }
+  }.property("@each.isCompleted")
+
+
 });
